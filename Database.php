0afaec4acb900c69388a16d8b78c8f58a72409af
X Tech <sadeeshanaakash16@gmail.com>
2025-01-22 00:50:27 +0530

    Update Database.php

--- Database.php
+++ Database.php
@@ -5,5 +5,12 @@ $db_pass = "";
 $db_name ="Pet";
 $conn = "";
 
-$conn = mysqli_connect(
+$conn = mysqli_connect($db_server,$db_user,$db_pass,$db_name);
+if($conn){
+  echo "Connected";
+}
+else{
+  echo"Can't Connect";
+}
+
 ?>
